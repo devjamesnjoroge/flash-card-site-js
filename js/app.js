4c81dde2b21a982d273aa7b9e888effab05bbfe2
devjamesnjoroge <james.njoroge@student.moringaschool.com>
2022-03-09 13:09:50 +0300

    ft: added cards with toggle effect

---
+++ js/app.js
@@ -0,0 +1,202 @@
+$(document).ready(function () {
+  $(".container:nth-child(1) .col-xl-3:nth-child(1) span").click(function () {
+    $(".container:nth-child(1) .col-xl-3:nth-child(1) span").css(
+      "display",
+      "none"
+    );
+    $(".container:nth-child(1) .col-xl-3:nth-child(1) p").css(
+      "display",
+      "flex"
+    );
+  });
+  $(".container:nth-child(1) .col-xl-3:nth-child(1) p").click(function () {
+    $(".container:nth-child(1) .col-xl-3:nth-child(1) p").css(
+      "display",
+      "none"
+    );
+    $(".container:nth-child(1) .col-xl-3:nth-child(1) span").css(
+      "display",
+      "flex"
+    );
+  });
+});
+$(document).ready(function () {
+  $(".container:nth-child(1) .col-xl-3:nth-child(2) span").click(function () {
+    $(".container:nth-child(1) .col-xl-3:nth-child(2) span").css(
+      "display",
+      "none"
+    );
+    $(".container:nth-child(1) .col-xl-3:nth-child(2) p").css(
+      "display",
+      "flex"
+    );
+  });
+  $(".container:nth-child(1) .col-xl-3:nth-child(2) p").click(function () {
+    $(".container:nth-child(1) .col-xl-3:nth-child(2) p").css(
+      "display",
+      "none"
+    );
+    $(".container:nth-child(1) .col-xl-3:nth-child(2) span").css(
+      "display",
+      "flex"
+    );
+  });
+});
+$(document).ready(function () {
+  $(".container:nth-child(1) .col-xl-3:nth-child(3) span").click(function () {
+    $(".container:nth-child(1) .col-xl-3:nth-child(3) span").css(
+      "display",
+      "none"
+    );
+    $(".container:nth-child(1) .col-xl-3:nth-child(3) p").css(
+      "display",
+      "flex"
+    );
+  });
+  $(".container:nth-child(1) .col-xl-3:nth-child(3) p").click(function () {
+    $(".container:nth-child(1) .col-xl-3:nth-child(3) p").css(
+      "display",
+      "none"
+    );
+    $(".container:nth-child(1) .col-xl-3:nth-child(3) span").css(
+      "display",
+      "flex"
+    );
+  });
+});
+
+//CONTAINER2
+$(document).ready(function () {
+  $(".container:nth-child(2) .col-xl-3:nth-child(1) span").click(function () {
+    $(".container:nth-child(2) .col-xl-3:nth-child(1) span").css(
+      "display",
+      "none"
+    );
+    $(".container:nth-child(2) .col-xl-3:nth-child(1) p").css(
+      "display",
+      "flex"
+    );
+  });
+  $(".container:nth-child(2) .col-xl-3:nth-child(1) p").click(function () {
+    $(".container:nth-child(2) .col-xl-3:nth-child(1) p").css(
+      "display",
+      "none"
+    );
+    $(".container:nth-child(2) .col-xl-3:nth-child(1) span").css(
+      "display",
+      "flex"
+    );
+  });
+});
+$(document).ready(function () {
+  $(".container:nth-child(2) .col-xl-3:nth-child(2) span").click(function () {
+    $(".container:nth-child(2) .col-xl-3:nth-child(2) span").css(
+      "display",
+      "none"
+    );
+    $(".container:nth-child(2) .col-xl-3:nth-child(2) p").css(
+      "display",
+      "flex"
+    );
+  });
+  $(".container:nth-child(2) .col-xl-3:nth-child(2) p").click(function () {
+    $(".container:nth-child(2) .col-xl-3:nth-child(2) p").css(
+      "display",
+      "none"
+    );
+    $(".container:nth-child(2) .col-xl-3:nth-child(2) span").css(
+      "display",
+      "flex"
+    );
+  });
+});
+$(document).ready(function () {
+  $(".container:nth-child(2) .col-xl-3:nth-child(3) span").click(function () {
+    $(".container:nth-child(2) .col-xl-3:nth-child(3) span").css(
+      "display",
+      "none"
+    );
+    $(".container:nth-child(2) .col-xl-3:nth-child(3) p").css(
+      "display",
+      "flex"
+    );
+  });
+  $(".container:nth-child(2) .col-xl-3:nth-child(3) p").click(function () {
+    $(".container:nth-child(2) .col-xl-3:nth-child(3) p").css(
+      "display",
+      "none"
+    );
+    $(".container:nth-child(2) .col-xl-3:nth-child(3) span").css(
+      "display",
+      "flex"
+    );
+  });
+});
+
+//CONTAINER3
+$(document).ready(function () {
+  $(".container:nth-child(3) .col-xl-3:nth-child(1) span").click(function () {
+    $(".container:nth-child(3) .col-xl-3:nth-child(1) span").css(
+      "display",
+      "none"
+    );
+    $(".container:nth-child(3) .col-xl-3:nth-child(1) p").css(
+      "display",
+      "flex"
+    );
+  });
+  $(".container:nth-child(3) .col-xl-3:nth-child(1) p").click(function () {
+    $(".container:nth-child(3) .col-xl-3:nth-child(1) p").css(
+      "display",
+      "none"
+    );
+    $(".container:nth-child(3) .col-xl-3:nth-child(1) span").css(
+      "display",
+      "flex"
+    );
+  });
+});
+$(document).ready(function () {
+  $(".container:nth-child(3) .col-xl-3:nth-child(2) span").click(function () {
+    $(".container:nth-child(3) .col-xl-3:nth-child(2) span").css(
+      "display",
+      "none"
+    );
+    $(".container:nth-child(3) .col-xl-3:nth-child(2) p").css(
+      "display",
+      "flex"
+    );
+  });
+  $(".container:nth-child(3) .col-xl-3:nth-child(2) p").click(function () {
+    $(".container:nth-child(3) .col-xl-3:nth-child(2) p").css(
+      "display",
+      "none"
+    );
+    $(".container:nth-child(3) .col-xl-3:nth-child(2) span").css(
+      "display",
+      "flex"
+    );
+  });
+});
+$(document).ready(function () {
+  $(".container:nth-child(3) .col-xl-3:nth-child(3) span").click(function () {
+    $(".container:nth-child(3) .col-xl-3:nth-child(3) span").css(
+      "display",
+      "none"
+    );
+    $(".container:nth-child(3) .col-xl-3:nth-child(3) p").css(
+      "display",
+      "flex"
+    );
+  });
+  $(".container:nth-child(3) .col-xl-3:nth-child(3) p").click(function () {
+    $(".container:nth-child(3) .col-xl-3:nth-child(3) p").css(
+      "display",
+      "none"
+    );
+    $(".container:nth-child(3) .col-xl-3:nth-child(3) span").css(
+      "display",
+      "flex"
+    );
+  });
+});
